ad6a77f552c0df88efe9e172cd5d52f037da0a99
match08 <match_2006@qq.com>
2020-01-07 16:53:21 +0800

    update demo version to ionic @ionic/angular 4x

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,36 +1,23 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { File } from '@ionic-native/file';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    File,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  schemas:[
-    NO_ERRORS_SCHEMA,
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ demo/src/app/home/home.page.ts
@@ -0,0 +1,143 @@
+import { Component, OnInit , NgZone} from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Zip } from 'capacitor-zip';
+import { FilesystemDirectory, FilesystemEncoding, Plugins } from '@capacitor/core';
+
+const {Filesystem} = Plugins;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage implements OnInit {
+  zip: Zip;
+  zipProgress: number = 0;
+  unzipProgress: number = 0;
+  hasFiles: boolean = false;
+  hasZip: boolean = false;
+  firstData = '';
+  secondData = '';
+  documentsDirectory = '';
+  constructor(public navCtrl: NavController, private zone:NgZone) {
+    this.zip = new Zip();
+  }
+
+  ngOnInit(): void {
+
+    this.zipProgress = 0;
+    this.unzipProgress =  0;
+    Filesystem.getUri({directory:FilesystemDirectory.Documents, path:'/'}).then(data=>{
+      this.documentsDirectory = data.uri;
+      this.checkForFiles();
+      this.checkForArchive();
+    })
+    .catch(err=>{
+      alert('Get documents directory error: ' + err);
+    });
+  }
+
+
+  async checkForArchive(){
+    try {
+      await Filesystem.stat({
+        directory: FilesystemDirectory.Documents,
+        path: 'testZip.zip'
+      });
+
+      this.zone.run(()=>{this.hasZip = true;});
+    } catch (e) {
+      console.log('checkForArchive', e);
+    }
+  }
+
+  async checkForFiles() {
+    try {
+      await Filesystem.stat({
+        directory: FilesystemDirectory.Documents,
+        path: 'toZip/first.txt'
+      });
+
+      await Filesystem.stat({
+        directory: FilesystemDirectory.Documents,
+        path: 'toZip/second.txt'
+      });
+
+      this.zone.run(()=>{this.hasFiles = true;});
+    } catch (e) {
+      console.log('checkForFiles', e);
+    }
+  }
+
+  async createFiles() {
+    try {
+
+      await Filesystem.mkdir({
+        directory: FilesystemDirectory.Documents,
+        path: 'toZip',
+        createIntermediateDirectories: true
+      });
+
+      await Filesystem.writeFile({
+        data: 'First File',
+        directory: FilesystemDirectory.Documents,
+        encoding: FilesystemEncoding.UTF8,
+        path: 'toZip/first.txt'
+      });
+
+      await Filesystem.writeFile({
+        data: 'Second File',
+        directory: FilesystemDirectory.Documents,
+        encoding: FilesystemEncoding.UTF8,
+        path: 'toZip/second.txt'
+      });
+      this.zone.run(()=>{
+        this.hasFiles = true;
+      });
+    } catch (e) {
+      console.log('createFiles', e);
+    }
+  }
+
+  async createZip() {
+    try {
+      alert(`${this.documentsDirectory}/testZip.zip`);
+      await this.zip.zip({
+        destination: `${this.documentsDirectory}/testZip.zip`,
+        source: `${this.documentsDirectory}/toZip`
+      }, (progress) => {
+        this.zone.run(()=>{this.zipProgress = progress.value;});
+      });
+      this.zone.run(()=>{
+        this.hasZip = true;
+      });
+    } catch (e) {
+      alert('createZip fail: '+ e);
+    }
+  }
+
+  async unzip() {
+    try {
+      await Filesystem.mkdir({
+        directory: FilesystemDirectory.Documents,
+        path: 'testZip',
+        createIntermediateDirectories: true
+      });
+      await this.zip.unZip({
+        source: `${this.documentsDirectory}/testZip.zip`,
+        destination: `${this.documentsDirectory}/testZip`,
+      }, (progress) => {
+        this.unzipProgress = progress.value;
+      });
+ 
+      this.firstData = (await Filesystem.readFile({directory: FilesystemDirectory.Documents, path: '/toZip/first.txt'})).data;
+      this.secondData = (await Filesystem.readFile({directory: FilesystemDirectory.Documents, path: '/toZip/second.txt'})).data;
+
+    } catch (e) {
+      alert('unzip fail: '+ e);
+    }
+  }
+
+
+}
