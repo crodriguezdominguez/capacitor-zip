4553647bfe7f9fc1db7630b197d3bd07605f6def
Allan Weir <allan@gojauntly.com>
2021-06-09 11:35:18 +0100

    and build

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -1,8 +1,3 @@
-declare module "@capacitor/core" {
-    interface PluginRegistry {
-        ZipPlugin: IZip;
-    }
-}
 export interface IZip {
     zip(options: ZipOptions, progress?: Function): Promise<ZipResult>;
     unZip(options: UnZipOptions, progress?: Function): Promise<UnzipResult>;
